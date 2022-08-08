f785110ab5e00a2a32c03bae88f6f17814c25ac3
Riley McCullough <rileybmccullough@gmail.com>
2022-08-07 18:07:48 -0600

    Fix: spelling error in file name

---
+++ lib/night_writer.rb
@@ -0,0 +1,7 @@
+
+file_read_data = File.open(ARGV[0], "r")
+file_read = file_read_data.read
+
+file_write = File.open(ARGV[1], "w")
+
+puts "Created #{ARGV[1]} containing #{file_read.length} characters"

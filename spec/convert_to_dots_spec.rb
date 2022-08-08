f2cc1fc7eb36398edeb5cce1a1e7e3c60b1213ed
Riley McCullough <rileybmccullough@gmail.com>
2022-08-08 16:31:17 -0600

    Test: can translate strings into arrays of braille

--- spec/convert_to_dots_spec.rb
+++ spec/convert_to_dots_spec.rb
@@ -7,9 +7,10 @@ RSpec.describe ConvertToDots do
     expect(braille_printer).to be_instance_of(ConvertToDots)
   end
 
-
-
-
+  it 'can translate string into braille arrays' do
+    b_printer = ConvertToDots.new
+    expect(b_printer.convert_this_text("hi")).to eq([["0.","00",".."], [".0","0.",".."]])
+  end
 
 
 end

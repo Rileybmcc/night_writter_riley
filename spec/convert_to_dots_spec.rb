97f5fc13b83835716e434a6cfa7fa05fd05a83ca
Riley McCullough <rileybmccullough@gmail.com>
2022-08-08 20:32:11 -0600

    Test: can reorder braille strings to be in three separate lines

--- spec/convert_to_dots_spec.rb
+++ spec/convert_to_dots_spec.rb
@@ -9,8 +9,16 @@ RSpec.describe ConvertToDots do
 
   it 'can translate string into braille arrays' do
     b_printer = ConvertToDots.new
-    expect(b_printer.convert_this_text("hi")).to eq([["0.","00",".."], [".0","0.",".."]])
+    expect(b_printer.convert_this_text("hi")).to eq(
+      [["0.","00",".."], [".0","0.",".."]])
   end
+
+  it 'can turn array of braille into three separate lines' do
+    b_printer = ConvertToDots.new
+    expect(b_printer.organize("hi")).to eq("0..0\n000.\n....")
+  end
+
+
 
 
 end

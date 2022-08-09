b331ebce5de2e566e8b93862a63958d8886171d2
Riley McCullough <rileybmccullough@gmail.com>
2022-08-09 15:29:54 -0600

    Refactor: can translate to english or braille

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,22 +1,23 @@
 require './lib/convert_to_dots'
+require './lib/convert_to_english'
 
 b_printer = ConvertToDots.new
+eng_printer = ConvertToEnglish.new
 
 file_read_data = File.open(ARGV[0], "r")
 file_read = file_read_data.read
 file_chr_length = file_read.each_char { |chr| "#{chr}" }.gsub("\n", "")
+indiv_chars = []
+
 file_write = File.open(ARGV[1], "w")
 
-puts "Created #{ARGV[1]} containing #{file_chr_length.length} characters"
 
-def txt_into_string(file)
-  new_string = ""
-  file.each_char  { |chr| new_string.concat "#{chr}" }
-  new_string
+file_chr_length = file_read.each_char { |chr| indiv_chars << "#{chr}" }
+
+if indiv_chars.uniq.count == 2 && indiv_chars.uniq.join == "0."" || "".0"
+  puts "Created #{ARGV[1]} containing #{file_chr_length.length} characters"
+  file_write.write(eng_printer.to_eng(file_read))
+else
+  puts "Created #{ARGV[1]} containing #{file_chr_length.length / 6} characters"
+  file_write.write(b_printer.organize(file_read))
 end
-
-# if file_read.uniq.count == 2 && file_read.uniq.to_s == ("0." || ".0")
-  # convert_to_english
-# else
-# require "pry"; binding.pry
-  file_write.write(b_printer.organize(txt_into_string(file_read)))

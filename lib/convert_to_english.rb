ec3d237c6a24472a7ef84be14ffdf8ce7590d979
Riley McCullough <rileybmccullough@gmail.com>
2022-08-09 11:49:42 -0600

    Feat: can read each line of text

--- lib/convert_to_english.rb
+++ lib/convert_to_english.rb
@@ -3,7 +3,13 @@ require './lib/translator'
 class ConvertToEnglish
   include Translator
 
-  
+  def file_by_line(data)
+    line_array = []
+    File.readlines(data).each do |line|
+        line_array << "#{line}"
+      end
+    line_array.map { |line| line.delete("\n") }
+  end
 
 
 

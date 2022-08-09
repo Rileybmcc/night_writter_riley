9383b5ed7d56c7a92406d2a53e4fd287ac7b6ce6
Riley McCullough <rileybmccullough@gmail.com>
2022-08-08 20:49:43 -0600

    Feat: can organize braille characters into propper format

--- lib/convert_to_dots.rb
+++ lib/convert_to_dots.rb
@@ -3,5 +3,26 @@ require './lib/translator'
 class ConvertToDots
   include Translator
 
+  def convert_this_text(input_string)
+    new_message = []
+    runon_string = input_string.downcase.delete(" ")
+    wasd = runon_string.each_char { |chr| new_message << braille_hash[chr] }
+    new_message
+  end
+
+  def organize(input_string)
+    letters = convert_this_text(input_string)
+    new_format = ""
+    num_lines = (letters.count.to_f / 40).ceil
+    num_lines.times do |index|
+      row_a = letters.map { |letter| letter[0] }
+      row_b = letters.map { |letter| letter[1] }
+      row_c = letters.map { |letter| letter[2] }
+      new_format.concat row_a.join.concat "\n".concat row_b.join.concat "\n".concat row_c.join
+      new_format.concat "\n" if num_lines != index + 1
+    end
+    new_format
+  end
+
 
 end

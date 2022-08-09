6d9dffeb3d989bcbc80c47c6e284ca64841915a7
Riley McCullough <rileybmccullough@gmail.com>
2022-08-08 21:34:45 -0600

    Feat: can instantiate ConvertToDots and translate file to braille

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,3 +1,6 @@
+require './lib/convert_to_dots'
+
+b_printer = ConvertToDots.new
 
 file_read_data = File.open(ARGV[0], "r")
 file_read = file_read_data.read
@@ -5,3 +8,12 @@ file_read = file_read_data.read
 file_write = File.open(ARGV[1], "w")
 
 puts "Created #{ARGV[1]} containing #{file_read.length} characters"
+
+
+def txt_into_string(file)
+  new_string = ""
+  file.each_char  { |chr| new_string.concat "chr" }
+  new_string
+end
+
+file_write.write(b_printer.organize(txt_into_string(file_read)))
